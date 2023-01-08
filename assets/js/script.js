72f934f2924f680153ad773cb29327fa21bd9609
ahsanaaxioned <ahsana@axioned.com>
2023-01-08 17:45:47 +0530

    js modified

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,16 @@
 // global variable declaration strat here
+console.clear();
 const form = document.querySelector(".my-form"),
   hamburger = document.querySelector(".hamburger"),
   navbar = document.querySelector(".navbar"),
   btn = document.querySelector(".btn"),
-  wrapper = document.querySelector(".wrapper")
+  wrapper = document.querySelector(".wrapper"),
   login = document.querySelector(".login"),
   email = document.querySelector(".username"),
   password = document.querySelector(".Password"),
   usernameRegex = /^([A-za-z-0-9._]{4,})([A-za-z\@?\s]{1,})/,
   passwordRegex = /^[a-zA-Z0-9!@#$%^&*]{5,16}$/,
   emailRegex = /^([A-Za-z0-9]{3,})\@([A-Za-z]{3,})\.([A-Za-z]{2,6})$/,
-  isLogin = false
   user_name = "teamaxioned",
   email_login = "axioned@gmail.com",
   passLogin = "prdxn@0442022",
@@ -20,11 +20,14 @@ const form = document.querySelector(".my-form"),
     usernameData: user_name,
     emailData: email_login,
     passwordData: passLogin,
-    is_login : isLogin
   };
-let array = [];
-
+console.log(data);
+let isLogin = false,
+  is_login = JSON.parse(localStorage.getItem("isLogin")),
+  array = [];
+console.log(is_login);
 // global variable declaration end here
+
 // hamburger event start here
 if (hamburger) {
   hamburger.addEventListener("click", () => {
@@ -34,29 +37,31 @@ if (hamburger) {
   });
 };
 // hamburger event end here
+
 array.push(obj);
-const ls = JSON.stringify(array);
-localStorage.setItem("userlogin", ls);
+const userData = JSON.stringify(array);
+localStorage.setItem("userlogin", userData);
+localStorage.setItem("isLogin", isLogin);
 
 // submit event start here
-if (login) {
-  login.addEventListener("click", (e) => {
+if (form) {
+  form.addEventListener("submit", (e) => {
     e.preventDefault();
     const fail = document.querySelectorAll(".fail");
     if ((email.value) && (password.value) && (fail.length === 0)) {
       // setting href to home page here
-      data.forEach((element, index) => {
-        if (((email.value === element.usernameData) ||
-          (email.value === element.emailData))
-          && ((password.value === element.passwordData))) {
+      if (((email.value === data[0].usernameData) ||
+        (email.value === data[0].emailData))
+        && ((password.value === data[0].passwordData))) {
           window.location.href = "./index.html";
-        }else{
-          const failMsg = document.createElement("span");
-          failMsg.className = "wrong";
-          failMsg.innerText = "please enter a valid details"
-          wrapper.insertBefore(failMsg,wrapper.children[1]);
-        }
-      });
+          isLogin = true;
+          localStorage.setItem("isLogin", isLogin);
+      } else {
+        const failMsg = document.createElement("span");
+        failMsg.className = "wrong";
+        failMsg.innerText = "please enter a valid details"
+        wrapper.insertBefore(failMsg, wrapper.children[1]);
+      }
     } else {
       validate(email, usernameRegex, emailRegex);
       validate(password, passwordRegex);
@@ -65,12 +70,14 @@ if (login) {
 };
 // submit event end here
 // button event start here for redirect to login page
-if(btn){
-  btn.addEventListener("click",()=>{
-    window.location.href= "http://127.0.0.1:5500/login.html";
+if (btn) {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    window.location.href = "/login.html";
+    localStorage.setItem("isLogin", isLogin);
   });
-  };
-  // button event end here for redirect to login page
+};
+// button event end here for redirect to login page
 // function for validating start here
 function validate(field, userRegEx, emailRegEx) {
   const inputGroup = field.parentElement;
